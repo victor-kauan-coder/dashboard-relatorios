463e11d16b77a1360a690c3a9e3170be7ce9f6c4
victor-kauan-coder <victor.kauan@ufpi.edu.br>
2025-10-05 18:38:48 -0300

    logo do site

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 import sys
 
 # --- CONFIGURAÇÕES ---
-st.set_page_config(page_title="Dashboard de Relatórios", layout="wide")
+st.set_page_config(page_title="Dashboard de Relatórios", layout="wide",page_icon="pet-logo.png")
 URL_DA_PLANILHA = "https://docs.google.com/spreadsheets/d/1foi-8QcpCwqgB_MkFKRyZ-FXFWAGQ4eUgHz2uqO2VvU/edit?resourcekey=&gid=526405481#gid=526405481"
 
 # --- FUNÇÃO PARA CARREGAR DADOS (COM LÓGICA PARA ONLINE E LOCAL) ---
